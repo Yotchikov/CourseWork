fb95dc27a675d0f81cc52422c978140005cd12cc
Yotchikov <books.antony@gmail.com>
2019-04-07 01:10:41 +0300

    Added simple add-in with graphviz4net package

---
+++ src/VisioAddIn/VisioAddIn/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// Управление общими сведениями о сборке осуществляется с помощью следующего 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("VisioAddIn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VisioAddIn")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
+[assembly: Guid("1c1ba31a-c46d-4ab1-88ef-1e52c21f9259")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
---
+++ src/VisioAddIn/VisioAddIn/ThisAddIn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Visio = Microsoft.Office.Interop.Visio;
+using Office = Microsoft.Office.Core;
+
+namespace VisioAddIn
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            this.Application.Documents.Add("");
+
+            Visio.Documents visioDocs = this.Application.Documents;
+            Visio.Document visioStencil = visioDocs.OpenEx("Basic Shapes.vss",
+                (short)Microsoft.Office.Interop.Visio.VisOpenSaveArgs.visOpenDocked);
+
+            Visio.Page visioPage = this.Application.ActivePage;
+
+            Visio.Master visioRectMaster = visioStencil.Masters.get_ItemU(@"Rectangle");
+            Visio.Shape visioRectShape = visioPage.Drop(visioRectMaster, 4.25, 5.5);
+            visioRectShape.Text = @"Rectangle text.";
+
+            Visio.Master visioHexagonMaster = visioStencil.Masters.get_ItemU(@"Hexagon");
+            Visio.Shape visioHexagonShape = visioPage.Drop(visioHexagonMaster, 7.0, 5.5);
+            visioHexagonShape.Text = @"Hexagon text.";
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Код, автоматически созданный VSTO
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
