58d26ec13660e2e6fab285359221d059aa34b641
Yotchikov <books.antony@gmail.com>
2019-04-07 18:13:02 +0300

    Added ribbon and first dot-to-shape mechanics in ThisAddIn.cs

---
+++ src/VisioAddIn/VisioAddIn/Ribbon.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace VisioAddIn
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileButton = this.Factory.CreateRibbonButton();
+            this.group = this.Factory.CreateRibbonGroup();
+            this.tab1.SuspendLayout();
+            this.group.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group);
+            this.tab1.Label = "Импорт графа";
+            this.tab1.Name = "tab1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Label = "Выбрать файл";
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.openFileButton_Click);
+            // 
+            // group
+            // 
+            this.group.Items.Add(this.openFileButton);
+            this.group.Name = "group";
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.Visio.Drawing";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group.ResumeLayout(false);
+            this.group.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton openFileButton;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
--- src/VisioAddIn/VisioAddIn/ThisAddIn.cs
+++ src/VisioAddIn/VisioAddIn/ThisAddIn.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Xml.Linq;
 using Visio = Microsoft.Office.Interop.Visio;
 using Office = Microsoft.Office.Core;
+using Graphviz4Net.Dot.AntlrParser;
+using Graphviz4Net.Dot;
 
 namespace VisioAddIn
 {
@@ -12,25 +14,55 @@ namespace VisioAddIn
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            this.Application.Documents.Add("");
-
-            Visio.Documents visioDocs = this.Application.Documents;
-            Visio.Document visioStencil = visioDocs.OpenEx("Basic Shapes.vss",
-                (short)Microsoft.Office.Interop.Visio.VisOpenSaveArgs.visOpenDocked);
-
-            Visio.Page visioPage = this.Application.ActivePage;
-
-            Visio.Master visioRectMaster = visioStencil.Masters.get_ItemU(@"Rectangle");
-            Visio.Shape visioRectShape = visioPage.Drop(visioRectMaster, 4.25, 5.5);
-            visioRectShape.Text = @"Rectangle text.";
-
-            Visio.Master visioHexagonMaster = visioStencil.Masters.get_ItemU(@"Hexagon");
-            Visio.Shape visioHexagonShape = visioPage.Drop(visioHexagonMaster, 7.0, 5.5);
-            visioHexagonShape.Text = @"Hexagon text.";
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
+        }
+
+        private DotGraph<string> ParseGraphData(string code)
+        {
+            var parser = AntlrParserAdapter<string>.GetParser();
+            var result = parser.Parse(
+                @"
+                digraph {
+                    node [label=""\N""];
+                    graph [bb=""0,0,74,112""];
+                    Hello [pos=""37,93"", width=""0.91667"", height=""0.52778""];
+                    World [pos=""37,19"", width=""1.0278"", height=""0.52778""];
+                    Hello -> World [pos=""e,37,38.249 37,73.943 37,66.149 37,56.954 37,48.338""];
+                }");
+
+            return result;
+        }
+
+        public void ShowGraph(string input)
+        {
+
+            Visio.Documents visioDocs = this.Application.Documents;
+            Visio.Document visioStencil = visioDocs.OpenEx("Basic Shapes.vss",
+                (short)Microsoft.Office.Interop.Visio.VisOpenSaveArgs.visOpenDocked);
+            Visio.Page visioPage = this.Application.ActivePage;
+
+            DotGraph<string> graph = ParseGraphData(input);
+
+            Visio.Master visioRectMaster = visioStencil.Masters.get_ItemU(@"Rectangle");
+            List<Visio.Shape> vertices = new List<Visio.Shape>();
+            
+            for (int i = 0; i < graph.AllVertices.Count(); ++i)
+            {
+                vertices.Add(visioPage.Drop(visioRectMaster, i, 11-i));
+                vertices[i].Text = graph.AllVertices.ElementAt(i).Id;
+            }
+
+            /*
+            Visio.Master visioRectMaster = visioStencil.Masters.get_ItemU(@"Rectangle");
+            Visio.Shape visioRectShape = visioPage.Drop(visioRectMaster, 0, 0);
+            visioRectShape.Text = @"Rectangle text.";
+
+            Visio.Master visioHexagonMaster = visioStencil.Masters.get_ItemU(@"Hexagon");
+            Visio.Shape visioHexagonShape = visioPage.Drop(visioHexagonMaster, 7.0, 5.5);
+            visioHexagonShape.Text = @"Hexagon text.";*/
         }
 
         #region Код, автоматически созданный VSTO
